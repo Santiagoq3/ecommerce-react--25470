6174434b66078990155ef688327c19d6ffed2b70
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-02-22 02:41:08 -0300

    navbar agregado

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.css"
+export const Navbar = () => {
+  return (
+    <header className='header'>
+        <h1 className='header-brand-name'>Tupac shakur - fake store</h1>
+        <nav className='header-nav'>
+           <ul>
+               <li><a href=''>Musicaa</a></li>
+               <li><a href=''>Vestir</a></li>
+               <li><a href=''>Accesorios</a></li>
+           </ul>
+        </nav>
+    </header>
+  )
+}
