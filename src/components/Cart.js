cfe67640488aa0ec20189b8e4ec19b8f5e7f5476
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-03-20 16:55:00 -0300

    cart componente agregado

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { cartContext } from '../context/CartContext'
 import "./cart.css"
+
 export const Cart = () => {
-  return (
-    <div>este es mi carrito</div>
-  )
+
+    const {cart} = useContext(cartContext)
+    console.log(Cart)
+    return (
+        <div className='cart'>
+            <h1>Shopping Cart</h1>
+            {
+                cart.length > 0 ?
+                <ul>
+                   { cart.map(item => {
+                      return <li className='cart_container'>
+                          <div className='cart_container-top'>
+                            <p className='cart_item-nombre'>camiseta</p>
+                            <p>$ 0</p>
+                          </div>
+                            <p>descripcion: 2PAC</p>
+                            <div className='cart_container-bottom'>
+                                <p>Cantidad: 0</p>
+                                <button>Remove</button>
+                            </div>
+                        </li>
+                    })}
+                    <div className='cart_comprar'>
+                        <div className='cart_comprar_total'>
+                            <p>Total</p>
+                            <p>$0</p>
+                        </div>
+                        <p className='cart_btn-comprar'>Generar orden</p>
+                        <Link to="/" className='cart_continuar-comprando' >
+                            <p style={{color: "black"}}>o Continuar Comprando</p> 
+                        </Link>
+                    </div>
+                </ul>
+                :
+                <div className='cart_vacio'>
+                     <p>No hay productos en el carrito</p>
+                     <Link to="/">Volver al home</Link>
+
+                </div>
+                
+            }
+        </div>
+    )
 }
